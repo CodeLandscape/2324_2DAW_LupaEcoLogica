e78c38fdef7497d10683ea51ead34f30a188eb43
SrIzquierdo <146435002+SrIzquierdo@users.noreply.github.com>
2023-11-14 10:19:39 +0100

    Base de datos v1.4
    No funcionaron los CHECK en el servidor, no se los traga

--- sql/baseDatos.sql
+++ sql/baseDatos.sql
@@ -11,67 +11,54 @@ CREATE TABLE partida(
   `puntuación` smallint UNSIGNED NOT NULL DEFAULT 0,
   `objetosAcertados` tinyint UNSIGNED NOT NULL,
   `preguntasAcertadas` tinyint UNSIGNED NOT NULL,
-  `fechaHora` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `fechaHora` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT pkPartida PRIMARY KEY (idPartida)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 CREATE TABLE categoria(
-  `idCategoria` tinyint UNSIGNED NOT NULL,
-  `nombre` varchar(20) NOT NULL
+  `idCategoria` tinyint UNSIGNED NOT NULL auto_increment,
+  `nombre` varchar(20) NOT NULL,
+  CONSTRAINT pkCategoria PRIMARY KEY (idCategoria)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 CREATE TABLE tablero(
-  `idTablero` tinyint UNSIGNED NOT NULL,
+  `idTablero` tinyint UNSIGNED NOT NULL auto_increment,
   `nombre` varchar(50) NOT NULL,
   `imagenFondo` blob NOT NULL,
-  `idCategoria` tinyint UNSIGNED
+  `idCategoria` tinyint UNSIGNED,
+  CONSTRAINT pkTablero PRIMARY KEY (idTablero)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 CREATE TABLE objeto(
-  `idObjeto` tinyint UNSIGNED NOT NULL,
+  `idObjeto` tinyint UNSIGNED NOT NULL auto_increment,
   `nombre` varchar(30) NOT NULL,
   `descripcion` varchar(120) NOT NULL,
   `imagen` blob NOT NULL,
   `puntuacion` tinyint UNSIGNED NOT NULL DEFAULT 10,
   `valoracion` bit NOT NULL,
-  `idCategoria` tinyint UNSIGNED
+  `idCategoria` tinyint UNSIGNED,
+  CONSTRAINT pkObjeto PRIMARY KEY (idObjeto)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 CREATE TABLE pregunta(
-  `idPregunta` tinyint UNSIGNED NOT NULL,
+  `idPregunta` tinyint UNSIGNED NOT NULL auto_increment,
   `texto` varchar(150) NOT NULL,
   `reflexionAcierto` varchar(255) NOT NULL,
   `reflexionFallo` varchar(255) NOT NULL,
   `puntuacion` tinyint UNSIGNED NOT NULL DEFAULT 100,
   `respuesta` bit NOT NULL,
-  `idCategoria` tinyint UNSIGNED
+  `idCategoria` tinyint UNSIGNED,
+  CONSTRAINT pkPregunta PRIMARY KEY (idPregunta)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
-ALTER TABLE config
-  ADD CONSTRAINT checkTiempo CHECK (tiempoCrono>=60);
-  ADD CONSTRAINT checkPreguntas CHECK (nPregunta>=3);
-  ADD CONSTRAINT checkObjetosBuenos CHECK (nObjetosBuenos<=6 AND nObjetosBuenos>=0);
-
-ALTER TABLE partida
-  ADD CONSTRAINT pkPartida PRIMARY KEY (idPartida);
-  ADD CONSTRAINT checkObjetoAcertado CHECK (objetosAcertados>=0);
-  ADD CONSTRAINT checkPreguntaAcertada CHECK (preguntasAcertadas>=0);
-
-ALTER TABLE categoria
-  ADD CONSTRAINT pkCategoria PRIMARY KEY (idCategoria);
-
 ALTER TABLE tablero
-  ADD CONSTRAINT pkTablero PRIMARY KEY (idTablero);
   ADD CONSTRAINT categoriaTablero FOREIGN KEY (idCategoria) REFERENCES categoria (idCategoria) ON DELETE CASCADE ON UPDATE CASCADE;
 
 ALTER TABLE objeto
-  ADD CONSTRAINT pkObjeto PRIMARY KEY (idObjeto);
   ADD CONSTRAINT categoriaObjeto FOREIGN KEY (idCategoria) REFERENCES categoria (idCategoria) ON UPDATE CASCADE;
-  ADD CONSTRAINT checkPuntuacion CHECK (puntuacion>=1);
 
 ALTER TABLE pregunta
-  ADD CONSTRAINT pkPregunta PRIMARY KEY (idPregunta);
   ADD CONSTRAINT categoriaPregunta FOREIGN KEY (idCategoria) REFERENCES categoria (idCategoria) ON DELETE CASCADE ON UPDATE CASCADE;
-  ADD CONSTRAINT checkPuntuacion CHECK (puntuacion>=1);
 
 CREATE UNIQUE INDEX categoriaNombre
   ON categoria (nombre);
@@ -81,9 +68,6 @@ CREATE UNIQUE INDEX tableroNombre
 
 CREATE UNIQUE INDEX tableroCategoria
   ON tablero (idCategoria);
-
-CREATE UNIQUE INDEX objetoNombre
-  ON objeto (nombre);
 
 CREATE UNIQUE INDEX objetoNombre
   ON objeto (nombre);
