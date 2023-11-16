9e47177659ff24ae1fa2db8388e29b1e43a32d15
SrIzquierdo <146435002+SrIzquierdo@users.noreply.github.com>
2023-11-16 17:12:39 +0100

    Modificación de addCategoria.php

--- src/php/vistas/addCategoria.php
+++ src/php/vistas/addCategoria.php
@@ -1,30 +1,53 @@
-<!--Aarón Izquierdo Cordero-->
-<html>
-    <head>
-        <meta charset=utf-8>
-    </head>
+<?php
+    include 'template/cabezera.html';
+?>
+		<title>Crear Categoría y Tablero</title>
+	</head>
     <body>
-        <form action="../controladores/addCategoria.php" method="post" enctype="multipart/form-data">
-            <p>
-                <label for="nomCategoria">Nombre de la Categoría: </label>
-                <input type="text" name="nomCategoria">
-            </p>
-            <p>
-                <label for="nomTablero">Nombre del Tablero: </label>
-                <input type="text" name="nomTablero">
-            </p>
-            <p>
-                <?php
-                    if(isset($_GET['error'])){
-                        if($_GET['error']=='1'){
-                            echo "<p>El archivo debe de ser una imagen de tipo PNG, JPG o JPEG.</p>";
+        <header>
+            Crear Categoría y Tablero
+        </header>
+        <main>
+            <form action="../controladores/crudTablero.php" method="post" enctype="multipart/form-data">
+                <div id="contenido">
+                    <label for='categoria'>Nombre de la categoría:</label>
+                    <input type='text' id='categoria' name='categoria' required><br>
+                    <?php
+                        if(isset($_GET['error'])){
+                            if($_GET['error']=='2'){
+                                echo "<p>El nombre de la categoría no puede ser uno existente.</p>";
+                            }
                         }
-                    }
-                ?>
-                <label for="fondo">Fondo</label>
-                <input type="file" name="fondo">
-            </p>
-            <input type="submit" value="Añadir">
-        </form>
-    </body>
-</html>
+                    ?>
+                    <label for='tablero'>Nombre del tablero:</label>
+                    <input type='text' id='tablero' name='tablero' required><br>
+                    <?php
+                        if(isset($_GET['error'])){
+                            if($_GET['error']=='3'){
+                                echo "<p>El nombre del tablero no puede ser uno existente.</p>";
+                            }
+                        }
+                    ?>
+                    <label for='img'>Fondo:</label>
+                    <input type='file' id='img' name='img' required><br>
+                    <?php
+                        if(isset($_GET['error'])){
+                            if($_GET['error']=='1'){
+                                echo "<p>El archivo debe de ser una imagen de tipo PNG, JPG o JPEG.</p>";
+                            }
+                            elseif($_GET['error']=='4'){
+                                echo "<p>Ha habido un error inexperado al realizar la acción, pruebe en otro momento. Si el problema persiste, llame a un administrador.</p>";
+                            }
+                        }
+                    ?>
+                    <input type="hidden" name="tipo" value="add">
+                    <div id="botones">
+                        <input type='submit' value='Añadir'>
+                        <a href="../index.php">Volver</a>
+                    </div>
+                </div>
+            </form>
+        </main>
+<?php
+    include 'template/pie.html';
+?>
