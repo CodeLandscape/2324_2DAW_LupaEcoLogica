64e7be0d1a05759467e02fbaf2053c3fae2512ba
SrIzquierdo <146435002+SrIzquierdo@users.noreply.github.com>
2023-11-17 10:07:30 +0100

    Update admin.php (Errores técnicos)
    En la tabla, coloqué mal las clases

--- src/php/vistas/admin.php
+++ src/php/vistas/admin.php
@@ -7,35 +7,36 @@
     <body>
         <header>
             Administración
-		<?php
-            		include 'template/navegacion.php';
-        	?>
+            <?php
+                include 'template/navegacion.php';
+            ?>
         </header>
 		<main>
-			<div id="contenido">
-					<!--Contenido de la tabla-->
-				<table>
-					<tr id="azul">
-						<td>Nombre</td>
-						<td>Ver</td>
-						<td>Borrar</td>
-					</tr>
-					<?php
-                        require '../controladores/controlador.php';
-                        $Control = new Controlador();
-                        $tabla = $Control->tablaCategoria();
-                        foreach($tabla as $fila){
-                            echo "<tr id=azul><td>".$fila['nombre']."</td>
-                                <td class=sinEstilo><a href=../index.php?id=".$fila['idCategoria']."&ruta=categoria><img src=img/IonEye.svg class=icono></a></td>
-                                <td class=sinEstilo><a href=../index.php?id=".$fila['idCategoria']."&ruta=removeCategoria><img src=img/IonIosRemoveCircle.svg class=icono></a></td>
-                            </tr>";
-                        }
-                    ?>
-				</table>
-				<a href="../index.php?ruta=addCategoria" class="submit">Añadir Categoría</a>
-			</div>
+            <form>
+                <div id="contenido">
+                        <!--Contenido de la tabla-->
+                    <table>
+                        <tr id="azul">
+                            <td>Nombre</td>
+                            <td>Ver</td>
+                            <td>Borrar</td>
+                        </tr>
+                        <?php
+                            require '../controladores/controlador.php';
+                            $Control = new Controlador();
+                            $tabla = $Control->tablaCategoria();
+                            foreach($tabla as $fila){
+                                echo "<tr><td id=azul>".$fila['nombre']."</td>
+                                    <td><a href=../index.php?id=".$fila['idCategoria']."&ruta=categoria class=sinEstilo><img src=../../img/IonEye.svg class=icono></a></td>
+                                    <td><a href=../index.php?id=".$fila['idCategoria']."&ruta=removeCategoria class=sinEstilo><img src=../../img/IonIosRemoveCircle.svg class=icono></a></td>
+                                </tr>";
+                            }
+                        ?>
+                    </table>
+                    <a href="../index.php?ruta=addCategoria" class="submit">Añadir Categoría</a>
+                </div>
+            </form>
 		</main>
 <?php
     include 'template/pie.html';
 ?>
-
