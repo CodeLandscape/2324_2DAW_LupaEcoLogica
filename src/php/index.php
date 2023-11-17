88d8a94cdd63acd1f8f85a25e3dd8e416cb9ff72
SrIzquierdo <146435002+SrIzquierdo@users.noreply.github.com>
2023-11-17 12:22:33 +0100

    Modificación de Index.php

--- src/php/index.php
+++ src/php/index.php
@@ -1,26 +1,23 @@
 <?php
     if(isset($_GET['ruta'])){
         $ruta=$_GET['ruta'];
-        switch ($ruta) {
-            case 'addCategoria':
-                if(!isset($_GET['error'])){$error=0;}
-                else{$error=$_GET['error'];}
-                header('Location:vistas/'.$ruta.'.php?error='.$error);
-                break;
-            case 'categoria':
-                $id=$_GET['id'];
-                header('Location:vistas/'.$ruta.'.php?id='.$id);
-                break;
-            case 'removeCategoria':
-                $id=$_GET['id'];
-                header('Location:vistas/'.$ruta.'.php?id='.$id);
-                break;
-            case 'modTablero':
-                $id=$_GET['id'];
-                if(!isset($_GET['error'])){$error=0;}
-                else{$error=$_GET['error'];}
-                header('Location:vistas/'.$ruta.'.php?id='.$id."&error=".$error);
-                break;
+        if(isset($_GET['error'])){
+            if(!isset($_GET['error'])){$error=0;}
+            else{$error=$_GET['error'];}
+            header('Location:vistas/'.$ruta.'.php?error='.$error);
+        }
+        elseif(isset($_GET['id'])){
+            $id=$_GET['id'];
+            header('Location:vistas/'.$ruta.'.php?id='.$id);
+        }
+        elseif(isset($_GET['id']) && isset($_GET['error'])){
+            $id=$_GET['id'];
+            if(!isset($_GET['error'])){$error=0;}
+            else{$error=$_GET['error'];}
+            header('Location:vistas/'.$ruta.'.php?id='.$id."&error=".$error);
+        }
+        else{
+            header('Location:vistas/'.$ruta.'.php');
         }
     }
     else{
