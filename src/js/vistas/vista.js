794ddb7b60f1aaf4517ce718b2590c6c9b5f6fca
Luis Tabares <ltabareslozano.guadalupe@alumnado.fundacionloyola.net>
2023-11-17 12:34:34 +0100

    Ultimo commit sprint1 Cliente

--- src/js/vistas/vista.js
+++ src/js/vistas/vista.js
@@ -1,27 +1,41 @@
-export class Vista{
-    static{
+/**
+ * Clase que representa una vista en la aplicación.
+ */
+export class Vista {
+    /**
+     * Enumeración de vistas disponibles.
+     * @type {Object}
+     * @readonly
+     */
+    static {
         Vista.VISTA1 = Symbol("Inicio")
         Vista.VISTA2 = Symbol("BusquedaObjetos")
         Vista.VISTA3 = Symbol("Preguntas")
         Vista.VISTA4 = Symbol("Registro")
-        Vista.VISTA5 = Symbol("Ranking")
+        Vista.VISTA5 = Symbol("Ranking")        // Símbolo para la vista de ranking
+    };
 
-    } 
-    constructor(controlador,base){
-        this.controlador=controlador
-        this.base = base
+    /**
+     * Crea una instancia de Vista.
+     * @param {any} controlador - El controlador asociado a la vista.
+     * @param {HTMLElement} base - El elemento base de la vista.
+     */
+    constructor(controlador, base) {
+        this.controlador = controlador;  // Asigna el controlador a la instancia de Vista
+        this.base = base;                // Asigna el elemento base a la instancia de Vista
     }
 
-    mostrar(ver)
-    {
-        if(ver)
-        
-            this.base.style.display="block"
-        
-        else
-            this.base.style.display="none"
-        
+    /**
+     * Muestra u oculta la vista según el valor de 'ver'.
+     * @param {boolean} ver - Valor booleano para mostrar u ocultar la vista.
+     */
+    mostrar(ver) {
+        if (ver) {
+            // Si 'ver' es verdadero, muestra el elemento
+            this.base.style.display = "block";
+        } else {
+            // Si 'ver' es falso, oculta el elemento
+            this.base.style.display = "none";
+        }
     }
-        
 }
-
