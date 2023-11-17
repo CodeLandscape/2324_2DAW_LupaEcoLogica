c9688b3848e14e2d4a0858aca7cef6b6f8112ecc
SrIzquierdo <146435002+SrIzquierdo@users.noreply.github.com>
2023-11-17 10:18:57 +0100

    Update navegacion.php (Error Técnico)

--- src/php/vistas/template/navegacion.php
+++ src/php/vistas/template/navegacion.php
@@ -1,6 +1,6 @@
         <nav>
 			<ul>
-				<li><a href="../../index.php">CATEGORÍAS</a></li>
+				<li><a href="../index.php">CATEGORÍAS</a></li>
 				<li><a href="#configuracion">CONFIGURACION</a></li>
 				<li><a href="#objetos">OBJETOS SIN CATEGORÍA</a></li>
 			</ul>
