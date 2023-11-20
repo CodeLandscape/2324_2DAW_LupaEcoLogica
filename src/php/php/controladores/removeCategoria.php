4f081eecd8906b74fd1d1b7dfe9b866592348ef4
Leandro Paniagua <you@example.com>
2023-11-20 10:59:28 +0100

    eliminar de carpeta duplicada php

--- src/php/php/controladores/removeCategoria.php
+++
@@ -1,9 +0,0 @@
-<?php
-    /* Aarón Izquierdo Cordero */
-    if(isset($_POST['idCategoria'])){
-        require '../modelos/modelo.php';
-        $Modelo=new Modelo();
-        $id=$_POST['idCategoria'];
-        $Modelo->borrarCategoria($id);
-        header("Location:../index.php");
-    }
