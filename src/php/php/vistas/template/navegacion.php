51abff6e40a1d1b5b485805033d5c70d92f3e0a9
Your Nameclear <you@example.com>
2023-11-20 17:53:12 +0100

    borrar php repetida

--- src/php/php/vistas/template/navegacion.php
+++
@@ -1,7 +0,0 @@
-        <nav>
-			<ul>
-				<li><a href="../index.php">CATEGORÍAS</a></li>
-				<li><a href="#configuracion">CONFIGURACION</a></li>
-				<li><a href="#objetos">OBJETOS SIN CATEGORÍA</a></li>
-			</ul>
-		</nav>
