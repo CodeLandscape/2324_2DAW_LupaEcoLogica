7e08414f9f5e690787bdb1d84d470dc0f663518c
Leandro Paniagua <leandrojosepaniaguabalbuena.guadalupe@alumnado.fundacionloyola.net>
2023-11-23 14:10:06 +0100

    cambios en el index y configdb

--- src/php/index.php
+++ src/php/index.php
@@ -1,26 +1,45 @@
 <?php
     require_once 'config/configBD.php';
-    require_once 'model/db.php';
+    require_once 'modelos/modelo.php';
+    
+    /** 
+     * Los ifs verifican si los parámetros controlador y accion están presentes en la URL. Si no están presentes, se asignan los
+     * valores por defecto definidos en las constantes CONTROLADOR_POR_DEFECTO y ACCION_POR_DEFECTO.
+    */
+    if (!isset($_GET["controlador"])) $_GET["controlador"] = constant("CONTROLADOR_POR_DEFECTO");
+    if (!isset($_GET["accion"])) $_GET["accion"] = constant("ACCION_POR_DEFECTO");
 
-    if (!isset($_GET["controller"])) $_GET["controller"] = constant("DEFAULT_CONTROLLER");
-    if (!isset($_GET["action"])) $_GET["action"] = constant("DEFAULT_ACTION");
-
-    $controller_path = 'controlador/' . $_GET["controller"] . '.php';
+    /**  
+     * Construye la ruta al archivo del controlador en función del parámetro controlador.
+     * Si el archivo del controlador no existe en la ruta especificada, utiliza el controlador por defecto.
+    */
+    $rutaControlador = 'controladores/' . $_GET["controlador"] . '.php';
 
     /* Verifica si existe el controlador */
-    if (!file_exists($controller_path)) $controller_path = 'controlador/' . constant("DEFAULT_CONTROLLER") . '.php';
-
-    /* Carga el controlador */
-    require_once $controller_path;
-    $nombreControlador = $_GET["controller"] . 'Controller';
+    if (!file_exists($rutaControlador)) $rutaControlador = 'controlador/' . constant("CONTROLADOR_POR_DEFECTO") . '.php';
+    
+    /**
+     * Incluye el archivo del controlador.
+     * Crea una instancia del controlador correspondiente.
+    */
+    require_once $rutaControlador;
+    $nombreControlador = $_GET["controlador"] . 'Controlador';
     $controlador = new $nombreControlador();
 
-    /* Verifica si el método está definido */
+    /**
+     * Verifica si el método (acción) especificado en la URL está definido en el controlador.
+     * Si está definido, ejecuta la acción y almacena los resultados en $datosAVista.
+    */
     $datosAVista["data"] = array();
-    if (method_exists($controlador, $_GET["action"])) $datosAVista["data"] = $controlador->{$_GET["action"]}();
+    if (method_exists($controlador, $_GET["accion"])) $datosAVista["data"] = $controlador->{$_GET["accion"]}();
 
-    /* Carga las vistas */
+    /**
+     * Carga las vistas.
+     * Incluye archivos de plantillas para la cabecera y el pie de la página.
+     * Incluye la vista específica del controlador.
+    */
     require_once 'vista/template/cabezera.php';
     require_once 'vista/' . $controlador->vista . '.php';
     require_once 'vista/template/pie.php';
+
 ?>
