249ae45e98600630ed39428fc0ae53d80766de63
Óscar Arroyo Aguadero <oscararroyoaguadero.guadalupe@alumnado.fundacionloyola.net>
2023-11-27 10:37:00 +0100

    Cambios en controlador y modelo. Añadido AJAX

--- src/php/controladores/controlador.php
+++ src/php/controladores/controlador.php
@@ -1,72 +1,112 @@
 <?php
-    require_once '../modelos/modelo.php';
+require_once '../modelos/modelo.php';
+
+/**
+ * Controlador para interactuar con la lógica de negocio y la presentación.
+ */
+class Controlador {
     /**
-     * Controlador para interactuar con la lógica de negocio y la presentación.
+     * Método que devuelve la tabla de categorías.
+     *
+     * @return array Un array bidimensional con las filas de la tabla categoría.
      */
-    class Controlador{
-        /**
-         * Método que devuelve la tabla de categorías.
-         *
-         * @return array Un array bidimensional con las filas de la tabla categoría.
-         */
-        function tablaCategoria(){
-            $Modelo = new Modelo();
-            $tabla=$Modelo->tablaCategoria();
-            return $tabla;
-        }
-        /**
-         * Método que devuelve el nombre de una categoría.
-         *
-         * @param int $id El ID de la categoría.
-         * @return string El nombre de la categoría.
-         */
-        function nombreCategoria($id){
-            $Modelo = new Modelo();
-            $fila=$Modelo->verCategoria($id);
-            return $fila['nombre'];
-        }
-        /**
-         * Método que devuelve el nombre del tablero de una categoría.
-         *
-         * @param int $idCategoria El ID de la categoría.
-         * @return string El nombre del tablero de la categoría.
-         */
-        function nombreTablero($idCategoria){
-            $Modelo = new Modelo();
-            $fila=$Modelo->verTablero($idCategoria);
-            return $fila['nombre'];
-        }
-        /**
-         * Método que devuelve la imagen de fondo del tablero de una categoría.
-         *
-         * @param int $idCategoria El ID de la categoría.
-         * @return string La ruta de la imagen de fondo del tablero.
-         */
-        function fondoTablero($idCategoria){
-            $Modelo = new Modelo();
-            $fila=$Modelo->verTablero($idCategoria);
-            return $fila['imagenFondo'];
-        }
-        /**
-         * Método que devuelve las filas de las preguntas de una categoría.
-         *
-         * @param int $idCategoria El ID de la categoría.
-         * @return array Un array bidimensional con las filas de la tabla pregunta.
-         */
-        function tablaPregunta($idCategoria){
-            $Modelo = new Modelo();
-            $tabla = $Modelo->verPreguntas($idCategoria);
-            return $tabla;
-        }
-        /**
-         * Método que devuelve las filas de los objetos de una categoría.
-         *
-         * @param int $idCategoria El ID de la categoría.
-         * @return array Un array bidimensional con las filas de la tabla objeto.
-         */
-        function tablaObjeto($idCategoria){
-            $Modelo = new Modelo();
-            $tabla = $Modelo->verObjetos($idCategoria);
-            return $tabla;
-        }
+    function tablaCategoria(){
+        $Modelo = new Modelo();
+        $tabla = $Modelo->tablaCategoria();
+        return $tabla;
     }
+
+    /**
+     * Método que devuelve el nombre de una categoría.
+     *
+     * @param int $id El ID de la categoría.
+     * @return string El nombre de la categoría.
+     */
+    function nombreCategoria($id){
+        $Modelo = new Modelo();
+        $fila = $Modelo->verCategoria($id);
+        return $fila['nombre'];
+    }
+
+    /**
+     * Método que devuelve el nombre del tablero de una categoría.
+     *
+     * @param int $idCategoria El ID de la categoría.
+     * @return string El nombre del tablero de la categoría.
+     */
+    function nombreTablero($idCategoria){
+        $Modelo = new Modelo();
+        $fila = $Modelo->verTablero($idCategoria);
+        return $fila['nombre'];
+    }
+
+    /**
+     * Método que devuelve la imagen de fondo del tablero de una categoría.
+     *
+     * @param int $idCategoria El ID de la categoría.
+     * @return string La ruta de la imagen de fondo del tablero.
+     */
+    function fondoTablero($idCategoria){
+        $Modelo = new Modelo();
+        $fila = $Modelo->verTablero($idCategoria);
+        return $fila['imagenFondo'];
+    }
+
+    /**
+     * Método que devuelve las filas de las preguntas de una categoría.
+     *
+     * @param int $idCategoria El ID de la categoría.
+     * @return array Un array bidimensional con las filas de la tabla pregunta.
+     */
+    function tablaPregunta($idCategoria){
+        $Modelo = new Modelo();
+        $tabla = $Modelo->verPreguntas($idCategoria);
+        return $tabla;
+    }
+
+    /**
+     * Método que devuelve las filas de los objetos de una categoría.
+     *
+     * @param int $idCategoria El ID de la categoría.
+     * @return array Un array bidimensional con las filas de la tabla objeto.
+     */
+    function tablaObjeto($idCategoria){
+        $Modelo = new Modelo();
+        $tabla = $Modelo->verObjetos($idCategoria);
+        return $tabla;
+    }
+
+    /**
+     * Método que devuelve las 10 filas con mayor puntuación de la tabla de partidas.
+     * 
+     * @return array Un array bidimensional con las filas de las partidas.
+     */
+    function rankingTabla(){
+        $Modelo = new Modelo();
+        $tabla = $Modelo->rankingTabla();
+        return $tabla;
+    }
+
+    /**
+     * Método que devuelve la configuración del juego.
+     * 
+     * @return array Un array con la configuración.
+     */
+    function configuracion(){
+        $Modelo = new Modelo();
+        $fila = $Modelo->configuracion();
+        return $fila;
+    }
+
+    /**
+     * Método que devuelve un tablero seleccionado aleatoriamente.
+     * 
+     * @return array Un array con la fila de la tabla Tablero seleccionada aleatoriamente.
+     */
+    function randomTablero(){
+        $Modelo = new Modelo();
+        $fila = $Modelo->randomTablero();
+        return $fila;
+    }
+}
+?>
